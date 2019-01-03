4b2f72f65401f3f14f4cd3f478a1a61f1e1f821b
Bradley <brad.driver33@gmail.com>
2019-01-03 13:58:06 -0500

    required all of the dependencies in server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+//Dependencies
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const mongoose = require('mongoose');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
